8a3ba814bb4430623a98997d59f18350974a6652
pdo9141 <pdo9141@gmail.com>
2016-12-05 21:57:04 -0800

    amplifyjs

---
+++ WebApp/js/utility.js
@@ -0,0 +1,18 @@
+﻿define([], function() {
+
+    var utility = {};
+
+    utility.submitForm = function () {
+        alert("submitForm");
+    };
+
+    utility.loadMorePosts = function (numOfPosts) {
+        alert("load " + numOfPosts + " posts");
+    }
+
+    return utility;
+
+});
+
+
+
--- WebApplication1/js/global.js
+++ WebApplication1/js/global.js
@@ -1,7 +1,29 @@
-﻿define(['jquery', 'toastr', 'utility'], function ($, toastr, Utils) {
-    $('h2#tester').html('Hello from the other side!');
-    toastr["success"]("Hello there!");
+﻿define(['jquery', 'toastr', 'utility', 'amplify'], function ($, toastr, Utils) {
 
+    /********* Basic JQuery and toastr test *****************
+    $('div#tester1').html('Hello from the other side!');
+    toastr["success"]("Hello there!");
+    *********************************************************/
+
+    /********* Use your own custom file as resource *********
     Utils.submitForm();
     Utils.loadMorePosts(9);
+    *********************************************************/
+
+    
+    var product = { name: "My Cookie Dough" };
+    amplify.store("product", product);
+    var item = amplify.store("product");
+    $('div#tester2').html('Object Product Name: ' + item.name);
+    
+    amplify.store("product1", "Bolt Cutter");
+    var value = amplify.store("product1");
+    $('div#tester3').html('KeyValue Product Name: ' + value);
+    
+    amplify.request.define("easyData", "ajax", {
+        url: "Data/easydata.txt",
+        dataType: "json",
+        type: "GET"
+    });
+
 });
