4bb6dfccefab23c6af5646e8ee539392b1bdb81d
Phillip <Phillip@WAYTOOAWESOME>
2016-12-05 19:52:34 -0800

    requirejs

---
+++ WebApplication1/js/config.js
@@ -0,0 +1,15 @@
+﻿// config.js
+
+requirejs.config({
+    baseUrl: 'js',
+    paths: {
+        jquery: [
+            'https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
+            'jsFrameworks/jquery-1.10.2.min'
+        ],
+        knockout: 'jsFrameworks/knockout-3.4.0',
+        toastr: 'jsFrameworks/toastr.min',
+        ladda: 'jsFrameworks/ladda.min',
+        utility: 'utility'
+    }
+});
---
+++ WebApplication1/js/global.js
@@ -0,0 +1,7 @@
+﻿define(['jquery', 'toastr', 'utility'], function ($, toastr, Utils) {
+    $('h2#tester').html('Hello from the other side!');
+    toastr["success"]("Hello there!");
+
+    Utils.submitForm();
+    Utils.loadMorePosts(9);
+});
