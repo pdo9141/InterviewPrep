e2ccdc1f60574209d91e692971b6e127434d5cae
Phillip <Phillip@WAYTOOAWESOME>
2017-02-26 23:18:24 -0800

    201702261118

---
+++ UnitTestProject1/Database/Question01/Queries.sql
@@ -0,0 +1,54 @@
+﻿USE TestDB
+
+--drop table Employees
+
+--Create table Employees
+--(
+--	ID int primary key identity,
+--	FirstName nvarchar(50),
+--	LastName nvarchar(50),
+--	Gender nvarchar(50),
+--	Salary int
+--)
+--GO
+
+--Insert into Employees values ('Ben', 'Hoskins', 'Male', 70000)
+--Insert into Employees values ('Mark', 'Hastings', 'Male', 60000)
+--Insert into Employees values ('Steve', 'Pound', 'Male', 45000)
+--Insert into Employees values ('Ben', 'Hoskins', 'Male', 70000)
+--Insert into Employees values ('Philip', 'Hastings', 'Male', 45000)
+--Insert into Employees values ('Mary', 'Lambeth', 'Female', 30000)
+--Insert into Employees values ('Valarie', 'Vikings', 'Female', 35000)
+--Insert into Employees values ('John', 'Stanmore', 'Male', 80000)
+--GO
+
+SELECT DISTINCT Salary FROM Employees WITH (NOLOCK) ORDER BY Salary DESC
+
+-- Get 2nd highest salary using subquery
+SELECT
+	TOP 1 Salary
+FROM (
+	SELECT
+		DISTINCT TOP 2 Salary
+	FROM Employees
+	ORDER BY Salary DESC
+) SubQueryResult
+ORDER BY Salary
+
+GO
+-- Get 2nd highest salary using CTE and DENSE_RANK
+WITH Result AS (
+	SELECT
+		Salary,
+		DENSE_RANK() OVER (ORDER BY Salary DESC)  AS DenseRank
+	FROM Employees
+)
+
+SELECT
+	TOP 1 Salary
+FROM Result 
+WHERE DenseRank = 2
+
+
+
+
---
+++ UnitTestProject1/Database/Question03/Queries.sql
@@ -0,0 +1,60 @@
+﻿USE TestDB
+
+--drop table Employees
+--GO 
+
+--Create table Employees
+--(
+--EmployeeID int primary key identity,
+--EmployeeName nvarchar(50),
+--ManagerID int foreign key references Employees(EmployeeID)
+--)
+--GO
+
+--Insert into Employees values ('John', NULL)
+--Insert into Employees values ('Mark', NULL)
+--Insert into Employees values ('Steve', NULL)
+--Insert into Employees values ('Tom', NULL)
+--Insert into Employees values ('Lara', NULL)
+--Insert into Employees values ('Simon', NULL)
+--Insert into Employees values ('David', NULL)
+--Insert into Employees values ('Ben', NULL)
+--Insert into Employees values ('Stacy', NULL)
+--Insert into Employees values ('Sam', NULL)
+--GO
+
+--Update Employees Set ManagerID = 8 Where EmployeeName IN ('Mark', 'Steve', 'Lara')
+--Update Employees Set ManagerID = 2 Where EmployeeName IN ('Stacy', 'Simon')
+--Update Employees Set ManagerID = 3 Where EmployeeName IN ('Tom')
+--Update Employees Set ManagerID = 5 Where EmployeeName IN ('John', 'Sam')
+--Update Employees Set ManagerID = 4 Where EmployeeName IN ('David')
+--GO
+
+SELECT * FROM Employees
+GO
+
+DECLARE @ID INT = 7;
+WITH EmployeeCTE AS (
+	-- Anchor
+	SELECT
+		EmployeeID, EmployeeName, ManagerID
+	FROM Employees		
+	WHERE EmployeeID = @ID
+
+	UNION ALL
+
+	-- Recursive Member
+	SELECT
+		Employees.EmployeeID, Employees.EmployeeName, Employees.ManagerID
+	FROM Employees		
+	INNER JOIN EmployeeCTE ON Employees.EmployeeID = EmployeeCTE.ManagerID
+)
+
+SELECT * FROM EmployeeCTE
+
+--SELECT 
+--	e1.EmployeeName,
+--	ISNULL(e2.EmployeeName, 'No Boss') AS ManagerName 
+--FROM EmployeeCTE e1
+--LEFT JOIN EmployeeCTE e2 ON e1.ManagerID = e2.EmployeeID
+	
