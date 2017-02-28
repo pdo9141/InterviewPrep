7439915dac6456dfc2eca4f960d7d7217c59e54a
Phillip <Phillip@WAYTOOAWESOME>
2017-02-27 21:35:27 -0800

    20170227935

---
+++ UnitTestProject1/Database/Question07/Queries.sql
@@ -0,0 +1,17 @@
+﻿USE TestDB
+
+--Create Table TestTable
+--(
+--ID int identity primary key,
+--Value nvarchar(50)
+--)
+
+--Insert into TestTable values ('123')
+--Insert into TestTable values ('ABC')
+--Insert into TestTable values ('DEF')
+--Insert into TestTable values ('901')
+--Insert into TestTable values ('JKL')
+
+SELECT * FROM TestTable
+
+SELECT Value FROM TestTable WHERE ISNUMERIC(Value) = 1
